8a8a1a7e05f95488ea128b94137a5740a54dbfda
khudadad rasikh <khudadadrasikh@khudadadsLaptop.fritz.box>
2022-11-02 08:10:50 +0100

    adding Mysql Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'rasikh123',
+    database: 'mysql',
+    entities: [],
+    synchronize: true,
+  }), TypeOrmModule.forFeature([Task]), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,16 @@
-export class Task {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  text: string;
+
+  @Column()
+  date: string;
+
+  @Column({ default: true })
+  status: boolean;
+}
