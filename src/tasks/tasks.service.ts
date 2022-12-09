79356a4ec716ac55144a8ef678f063d20a1ade11
khRasikh <kh.rasikh542@gmail.com>
2022-12-09 11:19:50 +0100

    chore: update tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepo: Repository<Task>
+  ){}
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
 
-  findAll() {
-    return `This action returns all tasks`;
+  findAll(): Promise<Task[]> {
+    console.log("HI")
+    return this.tasksRepo.find();
+  }
+ 
+  findOne(id: number): Promise<Task> {
+    return this.tasksRepo.findOneBy({ id });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
-  }
+
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
     return `This action updates a #${id} task`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: string): Promise<void> {
+    await this.tasksRepo.delete(id);
   }
 }
